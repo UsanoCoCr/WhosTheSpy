0af7fe189ace7cea516acb5a99da0545db73a7a5
cocr <forever.kzx0713@qq.com>
2023-08-23 18:47:12 +0800

    烂完了

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const OPENAI_API_KEY = '';
 
 const app = express();
 const PORT = 3000;
@@ -35,6 +36,8 @@ app.get('/start', (req, res) => {
     res.json(selectedWords);
 });
 
+let chatHistory = []; // 用于存储每个玩家的描述
+
 app.post('/chat', async (req, res) => {
     const playerName = req.body.playerName;
 
@@ -42,27 +45,79 @@ app.post('/chat', async (req, res) => {
     const gameContext = "在场5个人中4个人拿到相同的一个词语，剩下的1个拿到与之相关的另一个词语。每人每轮只能说一句话描述自己拿到的词语（不能直接说出那个词语），既不能让卧底发现，也要给同伴以暗示。";
 
     // 根据玩家名字和是否是卧底生成提示
-    const playerRole = playerName === undercover ? `你是卧底，你的词是 '${selectedWords.undercover}'。请描述你的词，但不要直接说出来。` : `你是平民，你的词是 '${selectedWords.common}'。请描述你的词，但不要直接说出来。`;
+    const playerRole = playerName === undercover ? `你是卧底，你的词是 '${selectedWords.undercover}'。` : `你是平民，你的词是 '${selectedWords.common}'。`;
 
-    const prompt = `${gameContext} ${playerRole}`;
+    const messages = [
+        { role: "system", content: gameContext },
+        ...chatHistory.map(chat => ({ role: "user", content: chat.content })), // 使用"user"作为role
+        { role: "user", content: playerRole + "请描述你的词，但不要直接说出来。" }
+    ];
 
     // 打印提示词
-    console.log(`Prompt for ${playerName}: ${prompt}`);
+    console.log(`Prompt for ${playerName}: ${playerRole}`);
 
     // 调用 ChatGPT API
-    const OPENAI_API_KEY = '';
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-        prompt: prompt,
-        max_tokens: 50,
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: "gpt-3.5-turbo",
+        messages: messages
     }, {
         headers: {
             'Authorization': `Bearer ${OPENAI_API_KEY}`
         }
     });
 
-    const reply = response.data.choices[0].text.trim();
+    const reply = response.data.choices[0].message.content.trim();
     console.log(`${playerName} said: ${reply}`);
+
+    // 将玩家的描述添加到聊天记录中
+    chatHistory.push({ content: `${playerName}: ${reply}` });
+
     res.json({ reply });
+});
+
+let votes = {}; // 用于存储每个玩家的票数
+
+app.post('/vote', async (req, res) => {
+    const playerName = req.body.playerName;
+
+    // 清空上一轮的投票结果
+    if (!votes[playerName]) {
+        votes = {
+            'Alice': 0,
+            'Bob': 0,
+            'Carol': 0,
+            'Dave': 0,
+            'Eve': 0
+        };
+    }
+
+    const prompt = `你是${playerName}，在场的玩家有Alice, Bob, Carol, Dave, Eve。请投票选择你认为是卧底的玩家。`;
+
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: "你正在玩谁是卧底游戏。" },
+            { role: "user", content: prompt }
+        ]
+    }, {
+        headers: {
+            'Authorization': `Bearer ${OPENAI_API_KEY}`
+        }
+    });
+
+    const vote = response.data.choices[0].message.content.trim();
+    for (let player of ['Alice', 'Bob', 'Carol', 'Dave', 'Eve']) {
+        if (vote.includes(player)) {
+            votes[player]++;
+        }
+    }
+
+    console.log(`${playerName} voted for: ${vote}`);
+    res.json({ vote });
+});
+
+app.get('/getVotes', (req, res) => {
+    res.json(votes);
 });
 
 app.listen(PORT, () => {
--- views/script.js
+++ views/script.js
@@ -1,4 +1,5 @@
 async function startGame() {
+     document.getElementById("startGameButton").style.display = "none";
     console.log("Starting game...");
     const response = await fetch('/start');
     const data = await response.json();
@@ -17,5 +18,37 @@ async function startGame() {
         const chatData = await chatResponse.json();
         const chatElem = document.getElementById(player.toLowerCase() + 'Chat');
         chatElem.innerHTML += `<p class="chat">${player}: ${chatData.reply}</p>`;
+    }
+    vote();
+}
+
+async function vote() {
+    const players = ['Alice', 'Bob', 'Carol', 'Dave', 'Eve'];
+    for (let player of players) {
+        const voteResponse = await fetch('/vote', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ playerName: player })
+        });
+
+        const voteData = await voteResponse.json();
+        const chatElem = document.getElementById(player.toLowerCase() + 'Chat');
+        chatElem.innerHTML += `<p class="chat">${player} voted for: ${voteData.vote}</p>`;
+    }
+
+    // 从后端获取投票结果
+    const votesResponse = await fetch('/getVotes');
+    const votes = await votesResponse.json();
+
+    // 判定胜负
+    const mostVotes = Math.max(...Object.values(votes));
+    const votedOut = Object.keys(votes).find(player => votes[player] === mostVotes);
+
+    if (votedOut === undercover) {
+        alert("平民胜利！");
+    } else {
+        alert("卧底胜利！");
     }
 }
